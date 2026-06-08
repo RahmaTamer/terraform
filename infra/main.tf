a2d737d6d1eb5536be439619f082a68117085fe4
Rahma Tamer <rahma.tamer67@gmail.com>
2026-06-08 12:58:52 +0300

    apdate5

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,15 @@
 resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
- instance_tenancy = "default"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
- tags = {
- "owner" = "vinod"
- Name = var.vpc_name
- }
+  tags = {
+    owner = "vinod"
+    Name  = var.vpc_name
+  }
 }
 
-
 resource "aws_subnet" "subnet" {
-  vpc_id     = aws_vpc.subnet.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -18,33 +17,20 @@ resource "aws_subnet" "subnet" {
   }
 }
 
-
-
-
-
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.gw.id
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "main"
   }
 }
-
-
-
-
 
 resource "aws_route_table" "r" {
-  vpc_id = aws_vpc.r.id
+  vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.foo.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
   }
 
   tags = {
@@ -52,11 +38,7 @@ resource "aws_route_table" "r" {
   }
 }
 
-
-
-
-
 resource "aws_route_table_association" "association" {
-  subnet_id      = aws_subnet.association.id
-  route_table_id = aws_route_table.association.id
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.r.id
 }
