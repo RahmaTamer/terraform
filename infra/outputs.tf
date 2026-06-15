d31ff7436ad94f6d3a20b448887fcf6da83f5cff
Rahma Tamer <rahma.tamer67@gmail.com>
2026-06-15 13:36:12 +0300

    Add terraform outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,17 @@
+# outputs.tf
+
 output "vpc_id" {
   value = aws_vpc.main.id
+}
+
+output "alb_dns" {
+  value = aws_lb.app_lb.dns_name
+}
+
+output "public_subnet_1" {
+  value = aws_subnet.public_subnet_1.id
+}
+
+output "public_subnet_2" {
+  value = aws_subnet.public_subnet_2.id
 }
